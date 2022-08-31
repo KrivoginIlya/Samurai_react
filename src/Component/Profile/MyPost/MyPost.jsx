e0482ad2edf35590208b554e0c4ab87ff7efb1ad
unknown <xline174@gmail.com>
2022-08-31 10:41:50 +0300

    props

--- src/Component/Profile/MyPost/MyPost.jsx
+++ src/Component/Profile/MyPost/MyPost.jsx
@@ -4,14 +4,17 @@ import mp from "./MyPost.module.css"
 
 
 const MyPost = ()=> {
+
     return (
+      <div>
+        <div>
+          <textarea name="sdasd" id="" cols="100" rows="10"></textarea>
+          <button>Send</button>
+        </div>
         <div className={mp.containerPost}>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+        <Post name='Jon Finger' message='Helloy' like='30'/>
+        <Post name='Lily Finger' message='Good day' like='425'/>
+      </div>
       </div>
     )
 }
